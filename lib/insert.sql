706217a264a7c20ce3cddfb9e7de6faf06b8b5b5
Ian Scott Martin <ianscottmartin@gmail.com>
2023-07-30 12:27:54 -0600

    lion and tigers and bears

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 22, "M", "choco brown", "ferocious", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 13, "M", "red", "playful", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 16, "F", "white", "lively", 0);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 41, "M", "black", "demanding", 1);
+INSERT INTO bears VALUES (5,"Melissa", 90, "F", "b/w", "quiet",  0);
+INSERT INTO bears VALUES (6,"Grinch", 33, "M", "green", "mischevious", 1);
+INSERT INTO bears VALUES (7,"Wendy", 3, "F", "brown", "sweet", 0);
+INSERT INTO bears VALUES (8,null, 33, "F", "black", "tame", 0);
+
+
